e0c64bc2c6e2a792a5ed0bcad9322c8fd7485b26
Mateusz Krajewski <maatsior@gmail.com>
2022-07-19 16:05:54 +0200

    Modify RentalHistoryService.java

--- src/main/java/model/RentalHistoryService.java
+++ src/main/java/model/RentalHistoryService.java
@@ -2,11 +2,7 @@ package model;
 
 import java.util.List;
 
-// TODO add class fields
-// TODO add constructor
 // TODO add getters and setters
-// TODO add equals and hashcode
-// TODO add toString
 public class RentalHistoryService {
 
     private final RentalHistoryRepository rentalHistoryRepository;
