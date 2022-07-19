7e5a08c7a86d6e0a2210aa7579b46b7e1d5442ba
Mateusz Krajewski <maatsior@gmail.com>
2022-07-19 15:25:51 +0200

    Initial commit

---
+++ src/main/java/app/CarRentalApp.java
@@ -0,0 +1,9 @@
+package app;
+
+class CarRentalApp {
+
+    public static void main(String[] args) {
+        CarRentalControl carRentalControl = new CarRentalControl();
+        carRentalControl.controlLoop();
+    }
+}
---
+++ src/main/java/app/CarRentalControl.java
@@ -0,0 +1,10 @@
+package app;
+
+class CarRentalControl {
+
+
+    // TODO implement method
+    void controlLoop() {
+        System.out.println("Test message");
+    }
+}
