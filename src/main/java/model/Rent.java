f17ec42d0eca3bf4da67f65f19941c9635e619c4
Mateusz Krajewski <maatsior@gmail.com>
2022-07-20 14:09:32 +0200

    Modify Rent.java

--- src/main/java/model/Rent.java
+++ src/main/java/model/Rent.java
@@ -8,7 +8,6 @@ import java.time.LocalDateTime;
 // TODO add class fields
 // TODO add constructor
 // TODO add getters and setters
-// TODO add equals and hashcode
 // TODO add toString
 public class Rent {
     private Customer customer;
