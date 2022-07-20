c525dd1eb662de25010b260d1697b456d61fea45
Mateusz Krajewski <maatsior@gmail.com>
2022-07-20 13:25:53 +0200

    Modify CarRentalControl.java

--- src/main/java/app/CarRentalControl.java
+++ src/main/java/app/CarRentalControl.java
@@ -1,7 +1,12 @@
 package app;
 
+import model.*;
+
 class CarRentalControl {
 
+    private final CarService carService = new CarService(new CarRepository());
+    private final CustomerService customerService = new CustomerService(new CustomerRepository());
+    private final RentalService rentalService = new RentalService();
 
     // TODO implement method
     void controlLoop() {
