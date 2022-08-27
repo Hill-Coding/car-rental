a15b7b5eb1c566c6ef53fea877cc80612149db74
Patryk Kawula <kawul1996@gmail.com>
2022-08-27 10:54:12 +0200

    Add method to create new Customer in io.DataReader

--- src/main/java/io/DataReader.java
+++ src/main/java/io/DataReader.java
@@ -1,8 +1,12 @@
 package io;
 
 import car.model.*;
+import customer.model.Address;
+import customer.model.Customer;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class DataReader {
@@ -79,5 +83,45 @@ public class DataReader {
 
         return new Car(brand, model, engine, gearbox, basePrice, vin,
                 productionYear, carType, carSegment, numberOfSeats, availability);
+    }
+
+    public Customer readAndCreateCustomer() {
+        System.out.println("Imię:");
+        String firstName = readString();
+
+        System.out.println("Nazwisko:");
+        String lastName = readString();
+
+        System.out.println("Email:");
+        String email = readString();
+
+        System.out.println("Numer telefonu:");
+        String phoneNumber = readString();
+
+        System.out.println("Data urodzenia (dzień-miesiąc-rok):");
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        LocalDate dateOfBirth = LocalDate.parse(readString(), dateTimeFormatter);
+
+        System.out.println("Numer prawa jazdy:");
+        String drivingLicense = readString();
+
+        System.out.println("Kraj pochodzenia:");
+        String country = readString();
+
+        System.out.println("Miasto:");
+        String city = readString();
+
+        System.out.println("Kod pocztowy (__-___):");
+        String zipCode = readString();
+
+        System.out.println("Ulica:");
+        String street = readString();
+
+        System.out.println("Numer budynku:");
+        String buildingNumber = readString();
+
+        Address customerAddress = new Address(country, city, zipCode, street, buildingNumber);
+        
+        return new Customer(firstName, lastName, email, phoneNumber, dateOfBirth, drivingLicense, customerAddress);
     }
 }
