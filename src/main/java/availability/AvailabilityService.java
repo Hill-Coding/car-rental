7fd605ba8101c58b0dd5e960f679e2f3a7663fa5
Mateusz Krajewski <maatsior@gmail.com>
2022-09-01 19:51:20 +0200

    Implement checkAvailability method in AvailabilityService

--- src/main/java/availability/AvailabilityService.java
+++ src/main/java/availability/AvailabilityService.java
@@ -5,38 +5,45 @@ import car.model.CarSegment;
 import rent.Rent;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 public class AvailabilityService {
 
-    // todo implement method
-    public static boolean checkAvailability(CarSegment carSegment, LocalDateTime from, LocalDateTime to) {
-        // check if there is at least one car available for requested dates
-        // if there is no available car return false
-        // if some car is available return true
-        throw new RuntimeException("Not implemented yet");
-    }
 
-    // todo implement method
-    public static boolean checkAvailability(Car car, LocalDateTime from, LocalDateTime to) {
-        // check if there is at least one car available for requested dates
-        // if there is no available car return false
-        // if some car is available return true
-        throw new RuntimeException("Not implemented yet");
+    // fixme lists should be injected, not initialized
+    private List<Car> cars = new ArrayList<>();
+    private List<Rent> rents = new ArrayList<>();
+
+    /**
+     * Checks if specific Car is available for requested dates range.
+     * @param car car to check
+     * @param from starting date
+     * @param to ending date
+     * @return boolean
+     */
+    public boolean checkAvailability(Car car, LocalDateTime from, LocalDateTime to) {
+        // todo check if method works
+        // todo write unit tests
+        return rents.stream()
+                .filter(rent -> rent.getCar().equals(car))
+                .noneMatch(rent -> isOverlapping(rent.getFrom(), from, rent.getTo(), to));
     }
 
     // todo implement method
     public static boolean checkAvailability(String vin, LocalDateTime from, LocalDateTime to) {
-        // check if there is at least one car available for requested dates
-        // if there is no available car return false
-        // if some car is available return true
+        // check if Car with specific VIN is available for requested dates
+        // should return true if it is available or else return false
+        // alternatively can return Optional with first car searched
         throw new RuntimeException("Not implemented yet");
     }
 
-    // maybe this should return list of available cars
-    public static boolean isAnyCarAvailableForDates(List<Rent> rents, LocalDateTime from, LocalDateTime to) {
-        return rents.stream()
-                .anyMatch(rent -> isOverlapping(rent.getFrom(), from, rent.getTo(), to));
+    // todo implement method
+    public List<Car> checkAvailability(CarSegment carSegment, LocalDateTime from, LocalDateTime to) {
+        // check if there is at least one car from specific segment available for requested dates
+        // should return list of available cars or empty list
+        // alternatively can return Optional with first car searched
+        throw new RuntimeException("Not implemented yet");
     }
 
     public static boolean isOverlapping(LocalDateTime start1, LocalDateTime end1, LocalDateTime start2, LocalDateTime end2) {
